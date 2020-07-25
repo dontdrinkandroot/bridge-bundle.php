964fb4a68b5ebd412bf09c87331d69e5dd6199f2
Philip Washington Sorst <philip@sorst.net>
2020-07-25 16:25:13 +0200

    Adding flex date

---
+++ Form/Type/FlexDateFormType.php
@@ -0,0 +1,86 @@
+<?php
+
+
+namespace Dontdrinkandroot\BridgeBundle\Form\Type;
+
+use Dontdrinkandroot\Date\FlexDate;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FlexDateFormType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add(
+                'year',
+                IntegerType::class,
+                ['label'    => false,
+                 'required' => false,
+                 'attr'     => ['class' => 'year', 'placeholder' => 'ddr.flex_date.year']
+                ]
+            )
+            ->add(
+                'month',
+                ChoiceType::class,
+                [
+                    'label'       => false,
+                    'required'    => false,
+                    'choices'     => $this->getMonthChoices(),
+                    'placeholder' => 'ddr.flex_date.month',
+                    'attr'        => ['class' => 'month']
+                ]
+            )
+            ->add(
+                'day',
+                ChoiceType::class,
+                [
+                    'label'       => false,
+                    'required'    => false,
+                    'choices'     => $this->getDayChoices(),
+                    'placeholder' => 'ddr.flex_date.day',
+                    'attr'        => ['class' => 'day']
+                ]
+            );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
+        $resolver->setDefaults(
+            [
+                'data_class' => FlexDate::class,
+                'attr'       => ['class' => 'flexdate']
+            ]
+        );
+    }
+
+    private function getMonthChoices()
+    {
+        $choices = [];
+        for ($i = 1; $i <= 12; $i++) {
+            $choices[$i] = $i;
+        }
+
+        return $choices;
+    }
+
+    private function getDayChoices()
+    {
+        $choices = [];
+        for ($i = 1; $i <= 31; $i++) {
+            $choices[$i] = $i;
+        }
+
+        return $choices;
+    }
+}
