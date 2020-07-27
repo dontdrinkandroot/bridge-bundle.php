55cd3a112b69150391551d03547d856455359ae9
Philip Washington Sorst <philip@sorst.net>
2020-07-27 20:22:19 +0200

    Adding Sonar, Tests

--- Tests/TestApp/config/bundles.php
+++ Tests/TestApp/config/bundles.php
@@ -1,5 +1,17 @@
 <?php
 
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class  => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class           => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class             => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class                     => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class            => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class            => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class    => ['test' => true],
+    Liip\TestFixturesBundle\LiipTestFixturesBundle::class           => ['test' => true],
+    Dontdrinkandroot\DoctrineBundle\DdrDoctrineBundle::class        => ['all' => true],
+    Dontdrinkandroot\ApiPlatformBundle\DdrApiPlatformBundle::class  => ['all' => true],
+    Dontdrinkandroot\CrudAdminBundle\DdrCrudAdminBundle::class      => ['all' => true],
+    Dontdrinkandroot\BootstrapBundle\DdrBootstrapBundle::class      => ['all' => true],
+    Dontdrinkandroot\BridgeBundle\DdrBridgeBundle::class            => ['all' => true],
 ];
