9500f58261ee8a646b8d21b385de6228b0477912
Philip Washington Sorst <philip@sorst.net>
2020-08-16 21:21:26 +0200

    Adding FontAwesome5BooleanRendererProvider

---
+++ Service/DdrCrudAdmin/FieldRenderer/FontAwesome5BooleanRendererProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Dontdrinkandroot\BridgeBundle\Service\DdrCrudAdmin\FieldRenderer;
+
+use Dontdrinkandroot\CrudAdminBundle\Model\FieldDefinition;
+use Dontdrinkandroot\CrudAdminBundle\Service\FieldRenderer\FieldRendererProviderInterface;
+
+/**
+ * @author Philip Washington Sorst <philip@sorst.net>
+ */
+class FontAwesome5BooleanRendererProvider implements FieldRendererProviderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function supports(FieldDefinition $fieldDefinition, $value): bool
+    {
+        return 'boolean' === $fieldDefinition->getType();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function render(FieldDefinition $fieldDefinition, $value): string
+    {
+        return $value ? '<span class="fas fa-fw fa-check"></span>' : '';
+    }
+}
