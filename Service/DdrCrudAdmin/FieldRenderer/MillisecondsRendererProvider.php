e86b94bbe783576315e1620f74831054931df694
Philip Washington Sorst <philip@sorst.net>
2021-12-22 17:19:19 +0100

    Adding MillisecondsRendererProvider

---
+++ Service/DdrCrudAdmin/FieldRenderer/MillisecondsRendererProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Dontdrinkandroot\BridgeBundle\Service\DdrCrudAdmin\FieldRenderer;
+
+use Dontdrinkandroot\Common\DateUtils;
+use Dontdrinkandroot\CrudAdminBundle\Model\FieldDefinition;
+use Dontdrinkandroot\CrudAdminBundle\Service\FieldRenderer\FieldRenderer;
+use Dontdrinkandroot\CrudAdminBundle\Service\FieldRenderer\FieldRendererProviderInterface;
+
+class MillisecondsRendererProvider implements FieldRendererProviderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function supports(FieldDefinition $fieldDefinition, $value): bool
+    {
+        return 'milliseconds' === $fieldDefinition->getType();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function render(FieldDefinition $fieldDefinition, $value): string
+    {
+        return FieldRenderer::escapeHtml(DateUtils::fromMillis($value)->format('Y-m-d H:i:s'));
+    }
+}
