81f27610f82add44254d1564ab5f54c74753119f
Philip Washington Sorst <philip@sorst.net>
2023-05-07 19:45:09 +0200

    tests: Testing Health Endpoint

---
+++ tests/Acceptance/HealthTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Dontdrinkandroot\BridgeBundle\Tests\Acceptance;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HealthTest extends WebTestCase
+{
+    public function testHealthWorking(): void
+    {
+        $client = self::createClient();
+        $client->request('GET', '/health');
+        self::assertResponseIsSuccessful();
+
+        $content = json_decode($client->getResponse()->getContent(), true);
+        self::assertEquals([
+            'http' => [
+                'base_uri' => 'http://localhost',
+                'host' => 'localhost',
+                'port' => 80,
+                'scheme' => 'http',
+                'secure' => false
+            ],
+            'database' => [
+                'default' => 'Doctrine\DBAL\Platforms\SqlitePlatform'
+            ]
+        ], $content);
+    }
+}
---
+++ tests/TestApp/config/routes.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Dontdrinkandroot\BridgeBundle\Tests\TestApp\Config;
+
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+
+return function (RoutingConfigurator $routes): void {
+    $routes->import('.', 'api_platform')->prefix('/api');
+    $routes->import('@DdrBridgeBundle/config/routes/health.yaml');
+};
