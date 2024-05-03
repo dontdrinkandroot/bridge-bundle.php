292719c827da9495d89b47d8d18877aa270d24ad
Philip Washington Sorst <philip@sorst.net>
2024-05-03 11:22:08 +0200

    refactor: Adding more icons

--- src/Model/BootstrapIcon.php
+++ src/Model/BootstrapIcon.php
@@ -4,13 +4,19 @@ namespace Dontdrinkandroot\BridgeBundle\Model;
 
 enum BootstrapIcon: string
 {
+    case CHECK = 'bi-check';
+    case DOT = 'bi-dot';
     case DOWNLOAD = 'bi-download';
+    case INCOGNITO = 'bi-incognito';
+    case MUSIC_NOTE_LIST = 'bi-music-note-list';
     case PENCIL = 'bi-pencil';
     case PLUS = 'bi-plus';
     case SEARCH = 'bi-search';
     case SHARE = 'bi-share';
     case THREE_DOTS_VERTICAL = 'bi-three-dots-vertical';
+    case UPLOAD = 'bi-upload';
     case TRASH = 'bi-trash';
+    case X = 'bi-x';
 
     public function toClassString(bool $fixedWith = false, array $additionalClasses = []): string
     {
